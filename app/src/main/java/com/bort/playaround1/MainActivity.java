85bc9880f4a5eab962fd62d41ddc18b36202097c
bort ske <gilbertchoy@gmail.com>
2020-11-08 20:15:47 -0800

    quicksort practice

--- app/src/main/java/com/bort/playaround1/MainActivity.java
+++ app/src/main/java/com/bort/playaround1/MainActivity.java
@@ -8,7 +8,8 @@ import android.util.Log;
 public class MainActivity extends AppCompatActivity {
     String tag = "berttest output";
     int ar[]=new int[7];  //mergeSort
-    int arr[]=new int[7];
+    int arr[]=new int[7];  //heapSort
+    static int array[] = new int[7];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,6 +55,18 @@ public class MainActivity extends AppCompatActivity {
         heapSort(arr);
         Log.d(tag,"heapsort:");
         printValues(arr);
+
+        //quickSort
+        array[0]=4;
+        array[1]=5;
+        array[2]=3;
+        array[3]=1;
+        array[4]=2;
+        array[5]=6;
+        array[6]=7;
+        quickSort(array, 0, ar.length-1);
+        Log.d(tag,"quicksort:");
+        printValues(array);
     }
 
     /*
@@ -176,7 +189,7 @@ public class MainActivity extends AppCompatActivity {
 
     */
 
-
+/*
     static void heapSort(int[] a){
         int n = a.length;
 
@@ -216,7 +229,7 @@ public class MainActivity extends AppCompatActivity {
         }
 
     }
-
+*/
 
 
     public void printValues(int a[]){
@@ -241,31 +254,31 @@ public class MainActivity extends AppCompatActivity {
 
     static void mergeSort(int[] a, int l, int r){
         if(l<r){
-          int m = (l+r)/2;
-          mergeSort(a,l,m);
-          mergeSort(a,m+1,r);
-          merge(a,l,m,r);
+            int m = l+r/2;
+            mergeSort(a,l,m);
+            mergeSort(a,m+1, r);
+            merge(a,l,m,r);
         }
     }
 
     static void merge(int[] a, int l, int m, int r){
-        int Lsize = m-l+1;
-        int Rsize = r-m;
+        int lSize = m-l+1;
+        int rSize = m - r;
 
-        int[] left = new int[m-l+1];
-        int[] right = new int[r-m];
+        int[] left = new int[lSize];
+        int[] right = new int[rSize];
 
-        for(int i=0; i<left.length; i++){
+        for(int i=0; i<lSize; i++){
             left[i] = a[l+i];
         }
-        for(int i=0; i<right.length; i++){
-            right[i] = a[m+i];
+        for(int i=0; i<rSize; i++){
+            right[i] = a[m+1+i];
         }
 
-        int lc=0;
-        int rc=0;
         int index = l;
-        while(lc<Lsize && rc<Rsize){
+        int lc = 0;
+        int rc = 0;
+        while(lc < lSize && rc < rSize){
             if(left[lc]<right[rc]){
                 a[index] = left[lc];
                 lc++;
@@ -276,16 +289,93 @@ public class MainActivity extends AppCompatActivity {
             }
             index++;
         }
-
-        while(lc<Lsize){
+        while(lc<lSize){
             a[index] = left[lc];
             lc++;
             index++;
         }
+        while(rc<rSize){
+            a[index] = right[rc];
+            rc++;
+            index++;
+        }
+    }
 
+    static void heapSort(int[] a){
+        int n = a.length;
 
+        for(int i = n/2-1; i>=0; i--){
+            heapify(a,n,i);
+        }
+
+        for(int i=n-1; i>0; i--){  //last var
+            int temp = a[0];
+            a[0] = a[i];
+            a[i]=temp;
+
+            heapify(a,i,0);
+        }
+    }
+
+    static void heapify(int[] a, int n, int i){ //n is length of array (length not including sorted part), i is the root
+        int largest = i;
+        int l = 2*i +1;
+        int r = 2*i +2;
+
+        if(l<n && a[l] > a[largest]){
+            largest = l;
+        }
+        if(r<n && a[r] > a[largest]){
+            largest = r;
+        }
+
+        if(largest !=i){
+            int swap = a[largest];
+            a[largest] = a[i];
+            a[i] = a[largest];
+
+            heapify(a,n,largest);
+        }
 
     }
 
+    public static void quickSort(int[] a, int l, int r){
+        if(l<r){
+            int pi = partition(a,l,r);
+            quickSort(a,l,pi -1);
+            quickSort(a,pi+1, r){
+            }
+        }
+    }
+
+    public static int partition(int[] a, int l, int r){
+        int pivot = a[r];
+        int i = l-1;
+        for(int j = l; j<r; j++){
+            i++;
+            if(a[j]<pivot){
+                int temp = a[j];
+                a[j] = a[i];
+                a[i]=temp;
+            }
+        }
+
+        //swap pivot at
+        int temp = a[i+1];
+        a[i+1] = a[r];
+        a[r] = temp;
+
+        return i+1;
+    }
+
+
+
+
+
 
 }
+
+
+
+
+
